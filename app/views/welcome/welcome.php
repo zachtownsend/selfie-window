20dd85458d68db6c9a6d92cd52c482ec015e7480
zachtownsend <zachtownsend@hotmail.co.uk>
2015-02-22 22:50:11 +0000

    Removed link on homepage

--- app/views/welcome/welcome.php
+++ app/views/welcome/welcome.php
@@ -2,5 +2,4 @@
 	<div class="content-container">
 		<img class="sizing-block" src="<?php echo \helpers\url::template_path() . '/img/sizing-block.png' ?>" />
 	</div>
-	<a href="http://goo.gl/nfY8wd" target="_blank" id="button-nav">Go to http://goo.gl/nfY8wd on your mobile to change Instagram image</a>
 </section>
